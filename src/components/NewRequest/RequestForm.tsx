2c3d76f04c3bee01e1e241e0cc2bca5c49fb899d
Hashininirasha <hashininirashamunasinghe@gmail.com>
2023-05-30 15:26:40 +0530

    added alert btn

--- src/components/NewRequest/RequestForm.tsx
+++ src/components/NewRequest/RequestForm.tsx
@@ -1,49 +1,91 @@
-
-import { Box, Button, IconButton, Paper, Table, TableBody, TableContainer, TableHead, TablePagination, TableRow, Tooltip, Typography } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add';
-import { EditOutlined } from '@mui/icons-material';
-import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import style from './RequestForm.module.scss'
-import { useNavigate } from 'react-router-dom'
-import { APP_ROUTES } from "../../utilities/constants/routes.constants"
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
-
-
+import { Alert, AlertTitle } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { APP_ROUTES } from '../../utilities/constants/routes.constants';
 
 const NewRequest = () => {
- 
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleClick = () => {
+    setShowAlert(true);
+  };
+
+  const handleAlertClose = () => {
+    setShowAlert(false);
+  };
+
+  const navigate = useNavigate()
+
+  const handleNewRequest = () => {
+
+    navigate(APP_ROUTES.REQUEST_CREATION)
+  }
+
   return (
-    <section className={style.gridContainer}>
-          <div className={style.gridHeader}>
-            <Typography
-              noWrap
-              component="div"
-              className={style.gridTitle}
-            >
-              <h3>Transport Request</h3>
-            </Typography>
-            <Box sx={{ flexGrow: 1 }} />
-            <div>
-              <Button className={style.gridActionButton} variant="contained">
+    <div>
+      <section className={style.gridContainer}>
+        <div className={style.gridHeader}>
+          <Typography
+            noWrap
+            component="div"
+            className={style.gridTitle}
+          >
+            <h3>Transport Request</h3>
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <div>
+            <Button className={style.gridActionButton} variant="contained">
               <Box>
-                  
-                    <div className='layout-row'>
-                      <Typography className={style.text}>
-                        Select saved templates
-                      </Typography>
-                      <KeyboardArrowDownOutlinedIcon className={style.icon} />
-                    </div>
-                 
-                </Box>
-              </Button>
-            </div>
+                <div className='layout-row'>
+                  <Typography className={style.text}>
+                    Select saved template
+                  </Typography>
+                  <KeyboardArrowDownOutlinedIcon className={style.icon} />
+                </div>
+              </Box>
+            </Button>
           </div>
-<hr></hr>
+        </div>
+        <hr />
+        <div className={style.gridHeader}>
+          <Box sx={{ flexGrow: 1 }} />
+          <div>
+            <Button className={style.sendRequestButton} variant="contained">
+              <Box>
+                <div className='layout-row'>
+                  <Typography className={style.text}
+  onClick={() => {
+    handleClick();
+
+    
+  }} >
+                    Send Request
+                  </Typography>
+                </div>
+              </Box>
+            </Button>
+          </div>
+        </div>
+      </section>
+       
+      {showAlert && (
+        <Alert severity="success" onClose={handleAlertClose} className={style.alertbox}   onClick={() => {
+         
+          handleNewRequest();
+      
           
-        </section>
-  )
-}
+        }}>
+          <AlertTitle>Requested Successfully</AlertTitle>
+       
+        </Alert>
+
+      )}
+    
+    </div>
+  );
+};
 
 export default NewRequest;
