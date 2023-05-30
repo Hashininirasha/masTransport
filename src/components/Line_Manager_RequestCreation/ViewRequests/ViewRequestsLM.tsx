99902334242d11f5c7901d457d579f531050e214
Hashininirasha <hashininirashamunasinghe@gmail.com>
2023-05-30 16:49:31 +0530

    added pagination

--- src/components/Line_Manager_RequestCreation/ViewRequests/ViewRequestsLM.tsx
+++ src/components/Line_Manager_RequestCreation/ViewRequests/ViewRequestsLM.tsx
@@ -1,14 +1,28 @@
-import { Box, Button, IconButton, Paper, Table, TableBody, TableContainer, TableHead, TableRow, Tooltip, Typography } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { EditOutlined } from '@mui/icons-material';
-import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import React, { useState } from 'react';
-import { StyledTableCell } from '../../../assets/theme/theme';
-import style from './ViewRequestsLM.module.scss';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Tooltip,
+  Typography
+} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { EditOutlined, RemoveRedEyeOutlined, DeleteOutlined } from '@mui/icons-material';
 import Avatar from '@mui/material/Avatar';
 import AvatarGroup from '@mui/material/AvatarGroup';
+import { Link } from 'react-router-dom';
+import { StyledTableCell } from '../../../assets/theme/theme';
+import style from './ViewRequestsLM.module.scss';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 const ViewRequestsLM = () => {
   const [rows, setRows] = useState([
@@ -52,12 +66,225 @@ const ViewRequestsLM = () => {
       Approver: ' Yoshani Jayathilaka',
       // Add more properties as per your requirements
     },
-    // Add more initial rows as needed
+    {
+      id: 1,
+      requestId: '12345',
+      requestType: 'Transportation',
+      createDate: '2023-05-27',
+      status: 'Pending',
+      VIPtrip: 'No',
+      From: 'Biyagama',
+      To : 'Pallekele',
+      Departure: '20/04/2023 8.00 AM',
+      Return : '22/04/2023 8.00 PM',
+      Passengers: [
+        {
+          id: 1,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 2,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 3,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 4,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+      ],
+      PreferredVehicle: 'Van',
+      Package: 'Yes',
+      CBM: 1,
+      Purpose: 'Traning Session',
+      Approver: ' Yoshani Jayathilaka',
+      // Add more properties as per your requirements
+    },
+    {
+      id: 1,
+      requestId: '12345',
+      requestType: 'Transportation',
+      createDate: '2023-05-27',
+      status: 'Pending',
+      VIPtrip: 'No',
+      From: 'Biyagama',
+      To : 'Pallekele',
+      Departure: '20/04/2023 8.00 AM',
+      Return : '22/04/2023 8.00 PM',
+      Passengers: [
+        {
+          id: 1,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 2,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 3,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 4,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+      ],
+      PreferredVehicle: 'Van',
+      Package: 'Yes',
+      CBM: 1,
+      Purpose: 'Traning Session',
+      Approver: ' Yoshani Jayathilaka',
+      // Add more properties as per your requirements
+    },
+    {
+      id: 1,
+      requestId: '12345',
+      requestType: 'Transportation',
+      createDate: '2023-05-27',
+      status: 'Pending',
+      VIPtrip: 'No',
+      From: 'Biyagama',
+      To : 'Pallekele',
+      Departure: '20/04/2023 8.00 AM',
+      Return : '22/04/2023 8.00 PM',
+      Passengers: [
+        {
+          id: 1,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 2,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 3,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 4,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+      ],
+      PreferredVehicle: 'Van',
+      Package: 'Yes',
+      CBM: 1,
+      Purpose: 'Traning Session',
+      Approver: ' Yoshani Jayathilaka',
+      // Add more properties as per your requirements
+    },
+    {
+      id: 1,
+      requestId: '12345',
+      requestType: 'Transportation',
+      createDate: '2023-05-27',
+      status: 'Pending',
+      VIPtrip: 'No',
+      From: 'Biyagama',
+      To : 'Pallekele',
+      Departure: '20/04/2023 8.00 AM',
+      Return : '22/04/2023 8.00 PM',
+      Passengers: [
+        {
+          id: 1,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 2,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 3,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 4,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+      ],
+      PreferredVehicle: 'Van',
+      Package: 'Yes',
+      CBM: 1,
+      Purpose: 'Traning Session',
+      Approver: ' Yoshani Jayathilaka',
+      // Add more properties as per your requirements
+    },
+    {
+      id: 1,
+      requestId: '12345',
+      requestType: 'Transportation',
+      createDate: '2023-05-27',
+      status: 'Pending',
+      VIPtrip: 'No',
+      From: 'Biyagama',
+      To : 'Pallekele',
+      Departure: '20/04/2023 8.00 AM',
+      Return : '22/04/2023 8.00 PM',
+      Passengers: [
+        {
+          id: 1,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 2,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 3,
+          name: 'Remy Sharp',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+        {
+          id: 4,
+          name: 'Travis Howard',
+          avatarSrc: 'https://th.bing.com/th/id/OIP.Fp7sFol83az5vls4n9eWtwHaEo?pid=ImgDet&rs=1',
+        },
+      ],
+      PreferredVehicle: 'Van',
+      Package: 'Yes',
+      CBM: 1,
+      Purpose: 'Traning Session',
+      Approver: ' Yoshani Jayathilaka',
+      // Add more properties as per your requirements
+    },
   ]);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   const handleDelete = (id: number) => {
     setRows((prevRows) => prevRows.filter((row) => row.id !== id));
   };
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPage(newPage);
+  };
+  
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
     <section className={style.gridContainer}>
@@ -71,9 +298,6 @@ const ViewRequestsLM = () => {
             <div className="layout-row">
               <AddIcon className={style.icon} />
               <Typography>New Request</Typography>
-              {/* <Link to="./new" className={style.text}>
-                New Request.
-              </Link> */}
             </div>
           </Button>
         </div>
@@ -82,7 +306,7 @@ const ViewRequestsLM = () => {
       <TableContainer component={Paper} className={style.grid}>
         <Table className={style.table}>
           <TableHead>
-            <TableRow>
+          <TableRow>
               <StyledTableCell width={120}>Request ID</StyledTableCell>
               <StyledTableCell width={120}>Request Type</StyledTableCell>
               <StyledTableCell width={180}>Create date and time</StyledTableCell>
@@ -105,68 +329,85 @@ const ViewRequestsLM = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
               <TableRow key={row.id}>
-                <StyledTableCell>{row.requestId}</StyledTableCell>
-                <StyledTableCell>{row.requestType}</StyledTableCell>
-                <StyledTableCell>{row.createDate}</StyledTableCell>
-                <StyledTableCell>{row.status}</StyledTableCell>
-                <StyledTableCell>{row.VIPtrip}</StyledTableCell>
-                <StyledTableCell>{row.From}</StyledTableCell>
-                <StyledTableCell>{row.To}</StyledTableCell>
-                <StyledTableCell>{row.Departure}</StyledTableCell>
-                <StyledTableCell>{row.Return}</StyledTableCell>
-                
-                <StyledTableCell className={style.actionColumn}>
-                  <AvatarGroup max={3}>
-                    {row.Passengers.map((passenger) => (
-                      <Avatar
-                        key={passenger.id}
-                        alt={passenger.name}
-                        src={passenger.avatarSrc}
-                      />
-                    ))}
-                  </AvatarGroup>
-                </StyledTableCell>
-                <StyledTableCell>{row.PreferredVehicle}</StyledTableCell>
-                <StyledTableCell>{row.Package}</StyledTableCell>
-                <StyledTableCell>{row.CBM}</StyledTableCell>
-                <StyledTableCell>{row.Purpose}</StyledTableCell>
-                <StyledTableCell>{row.Approver}</StyledTableCell>
-                <StyledTableCell className={style.actionColumn} style={{ backgroundColor: '#282828' }}>
-                  <Box className="layout-row">
-                    <Box>
-                      <IconButton size="small">
-                        <Tooltip title="View">
-                          <RemoveRedEyeOutlinedIcon sx={{ fontSize: '20px', mr: '-1', color: 'white' }} />
+              <StyledTableCell>{row.requestId}</StyledTableCell>
+              <StyledTableCell>{row.requestType}</StyledTableCell>
+              <StyledTableCell>{row.createDate}</StyledTableCell>
+              <StyledTableCell>{row.status}</StyledTableCell>
+              <StyledTableCell>{row.VIPtrip}</StyledTableCell>
+              <StyledTableCell>{row.From}</StyledTableCell>
+              <StyledTableCell>{row.To}</StyledTableCell>
+              <StyledTableCell>{row.Departure}</StyledTableCell>
+              <StyledTableCell>{row.Return}</StyledTableCell>
+              
+              <StyledTableCell className={style.actionColumn}>
+                <AvatarGroup max={3}>
+                  {row.Passengers.map((passenger) => (
+                    <Avatar
+                      key={passenger.id}
+                      alt={passenger.name}
+                      src={passenger.avatarSrc}
+                    />
+                  ))}
+                </AvatarGroup>
+              </StyledTableCell>
+              <StyledTableCell>{row.PreferredVehicle}</StyledTableCell>
+              <StyledTableCell>{row.Package}</StyledTableCell>
+              <StyledTableCell>{row.CBM}</StyledTableCell>
+              <StyledTableCell>{row.Purpose}</StyledTableCell>
+              <StyledTableCell>{row.Approver}</StyledTableCell>
+              <StyledTableCell className={style.actionColumn} style={{ backgroundColor: '#282828' }}>
+                <Box className="layout-row">
+                  <Box>
+                    <IconButton size="small">
+                      <Tooltip title="View">
+                        <RemoveRedEyeOutlinedIcon sx={{ fontSize: '20px', mr: '-1', color: 'white' }} />
+                      </Tooltip>
+                    </IconButton>
+                  </Box>
+                  <Box>
+                    <IconButton size="small">
+                    <Tooltip title="Edit">
+                          <EditOutlined sx={{ fontSize: '20px', mr: '-1', color: 'white' }} />
                         </Tooltip>
                       </IconButton>
                     </Box>
                     <Box>
-                      <IconButton size="small">
-                      <Tooltip title="Edit">
-                            <EditOutlined sx={{ fontSize: '20px', mr: '-1', color: 'white' }} />
-                          </Tooltip>
-                        </IconButton>
-                      </Box>
-                      <Box>
-                        <IconButton size="small" onClick={() => handleDelete(row.id)}>
-                          <Tooltip title="Delete">
-                            <DeleteOutlinedIcon sx={{ fontSize: '20px', mr: '-1', color: 'white' }} />
-                          </Tooltip>
-                        </IconButton>
-                      </Box>
+                      <IconButton size="small" onClick={() => handleDelete(row.id)}>
+                        <Tooltip title="Delete">
+                          <DeleteOutlinedIcon sx={{ fontSize: '20px', mr: '-1', color: 'white' }} />
+                        </Tooltip>
+                      </IconButton>
                     </Box>
-                  </StyledTableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          
-        </TableContainer>
-      </section>
-    );
-  };
-  
-  export default ViewRequestsLM;
-  
+                  </Box>
+                </StyledTableCell>
+              </TableRow>
+            ))}
+
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }} className={style.paginationReq}>
+                <TableCell colSpan={15} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+
+        <TablePagination className={style.paginationReq}
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </TableContainer>
+    </section>
+  );
+};
+
+export default ViewRequestsLM;
